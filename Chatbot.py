9680e9f768cd7ca541eaac1aa3fd9d2f663e113e
Diego L Malpica <38761542+strikerdlm@users.noreply.github.com>
2023-07-03 13:57:46 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,11 +1,31 @@
+import os
 import openai
+import requests
 import streamlit as st
 
+# Function to query the APIs
+def query(api_url, payload):
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+    except:
+        return None
+
+# Fetch the API URLs from environment variables
+api_urls = [
+    os.environ.get('API_URL_1'),
+    os.environ.get('API_URL_2'),
+    # ... add more as needed
+]
+
+# Streamlit Interface
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    
 
-st.title("Follow the white rabbit")
+st.title("FTWR")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Feed my Frankestein"}]
 
@@ -17,10 +37,23 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
+    # Query multiple APIs
+    outputs = []
+    for index, api_url in enumerate(api_urls, start=1):
+        if api_url:
+            output = query(api_url, {"question": prompt})
+            if output:
+                outputs.append(f"PC{index}: {output}")
+
+    # Concatenate outputs to be sent to OpenAI
+    fused_output = ' '.join(outputs)
+
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    
+    # Send concatenated outputs to OpenAI model
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages + [{"role": "system", "content": fused_output}])
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
