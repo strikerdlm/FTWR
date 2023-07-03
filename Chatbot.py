35cda6628e1fa1a5c15117f1e59b6f30aa5c4ef4
Diego L Malpica <38761542+strikerdlm@users.noreply.github.com>
2023-07-03 13:55:06 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,10 +3,9 @@ import streamlit as st
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    
 
-st.title("FTWR")
+st.title("Follow the white rabbit")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Feed my Frankestein"}]
 
