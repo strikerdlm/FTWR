b0fb7c1e521735189c2af8f9f045adb62b582246
Diego L Malpica <38761542+strikerdlm@users.noreply.github.com>
2023-07-03 14:30:48 -0500

    Update 1_File_Q&A.py

--- pages/1_File_Q&A.py
+++ pages/1_File_Q&A.py
@@ -3,10 +3,9 @@ import anthropic
 
 with st.sidebar:
     anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 File Q&A with Anthropic")
+
+st.title("Feed a File")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
     "Ask something about the article",
